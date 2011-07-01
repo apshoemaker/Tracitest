0b88e9ece490d5f703376f786ce456e78810423f
Ethan Pursley <plopmodz@gmail.com>
2011-07-01 15:56:46 -0700

    fixed comment

--- test.php
+++ test.php
@@ -5,15 +5,15 @@ require("deadband.php");
 /* Call our class */
 $tracitest = new Tracitest;
 
-/* Set 3 Alarms / Probes / temps */
+/* Set 1 Alarm / 3 Probes & Temps */
 $probes = array(rand(0,100), rand(0,100), rand(0,100));
 
 $tracitest->_alarm_set(1, 60);
 
-// Lets try to recreate an alarm just for fun
+/* Lets try to recreate an alarm just for fun */
 $tracitest->_alarm_set(1, 60);
 
-// comm_temp_fire these bad boys
+/* comm_temp_fire these bad boys */
 foreach($probes as $probe => $temp) {
 	echo "Probe {$probe}: ";
 	echo ($tracitest->comm_temp_fire($probe, $temp)) ? 'true' : 'false';
